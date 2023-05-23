52139a4fbd6616d391348f35bf40f139fb5fb8df
and1kmpo <acampolpez@gmail.com>
2023-05-23 16:29:31 -0500

    Desarrollo inicial

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+import colors from 'tailwindcss/colors';
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],
+  theme: {
+    colors: {
+      ...colors,
+      'primary': '#CC2D4A',
+      'secondary': '#8FA206',
+      'tertiary': '#61AEC9',
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif']},
+      backgroundImage: {
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('../img/yosemite.jpg')",
+        'LA': "url('../img/LA.jpg')",
+        'seattle': "url('../img/seattle.jpg')",
+        'new_york': "url('../img/new_york.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'sydney': "url('../img/sydney.jpg')",
+        'miami': "url('../img/miami.jpg')",
+        'switzerland': "url('../img/switzerland.jpg')",
+        'bali': "url('../img/bali.jpg')",
+        'chicago': "url('../img/chicago.jpg')",
+        'europe': "url('../img/europe.jpg')",
+        'iceland': "url('../img/iceland.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
